7af7e46b45732c088edfd79d853fb2ab35e70480
Joseph Cheek <joseph@ziquid.com>
2026-01-09 00:02:05 -0700

    switch timestamp env vars back to ZDS_AI_AGENT_.

--- src/lib/qcli.js
+++ src/lib/qcli.js
@@ -245,8 +245,8 @@ async function executeQCLI(query, agentName, authorUsername, channel, messageDat
   } else {
     const maxACL = getMaxACL(channel, debug);
     env.ZDS_AI_AGENT_MESSAGE_AUTHOR = authorUsername;
-    env.SOCIOBOT_MESSAGE_TIMESTAMP_UTC = messageDate.toISOString();
-    env.SOCIOBOT_MESSAGE_TIMESTAMP_LOCAL = messageDate.toLocaleString();
+    env.ZDS_AI_AGENT_MESSAGE_TIMESTAMP_UTC = messageDate.toISOString();
+    env.ZDS_AI_AGENT_MESSAGE_TIMESTAMP_LOCAL = messageDate.toLocaleString();
     env.ZDS_AI_AGENT_MESSAGE_ACL = currentACL.toString();
     env.ZDS_AI_AGENT_MESSAGE_MAX_ACL = maxACL.toString();
 
